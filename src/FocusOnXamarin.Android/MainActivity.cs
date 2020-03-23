4e5a89260eac0d95f6b1a375dd0ab8e060882000
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-03-23 15:43:30 +0100

    Small update in slides

--- src/FocusOnXamarin.Android/MainActivity.cs
+++ src/FocusOnXamarin.Android/MainActivity.cs
@@ -1,15 +1,16 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace FocusOnXamarin.Droid
 {
-    [Activity(Label = "FocusOnXamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(
+        Label = "FocusOnXamarin",
+        Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
